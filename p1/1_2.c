ad4270cd12a67dec778da9a827a5406bb2c7f851
이성민 <iseongmin@iseongmin-ui-MacBookPro.local>
2024-07-01 15:46:15 +0900

    1.1

---
+++ p1/1_2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2;
+    scanf("%d, %d", &num1, &num2);
+    int result = 0;
+    result = num1 + num2;
+    printf("%d", result);
+    return 0;
+}
---
+++ p1/1_2.py
@@ -0,0 +1,6 @@
+x = input() 
+y = input()
+x = int(x)
+y = int(y)
+z = x+y
+print(z)
