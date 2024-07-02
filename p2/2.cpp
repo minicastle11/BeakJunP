4b55d0f44aa9053c4103fd366f56cec6a84a1f64
이성민 <iseongmin@iseongmin-ui-MacBookPro.local>
2024-07-02 23:54:22 +0900

    2.1

--- p2/2.cpp
+++ p2/2.cpp
@@ -1,10 +1,13 @@
-#include <stdio.h>
+#include <iostream>
+
+using namespace std;
 
 int main()
 {
-    int num1, num2;
-    int result;
-    scanf("%d %d", &num1, &num2);
+    int num1, num2, result;
+
+    cin >> num1 >> num2;
+
     if (num1 < 0 && num2 < 0)
         result = 3;
     else if (num1 < 0 && num2 > 0)
@@ -13,6 +16,7 @@ int main()
         result = 1;
     else
         result = 4;
-    printf("%d", result);
+        
+    cout << result;
     return 0;
 }
