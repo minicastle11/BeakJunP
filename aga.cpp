8cc507b0caa7694453c089ce474196523fdbf964
이성민 <iseongmin@iseongmin-ui-MacBookPro.local>
2024-07-04 22:39:50 +0900

    2

---
+++ aga.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int a[3] = {3, 4, 1};
+
+    for (int i = 0; i < 3; i ++){
+        
+
+    }
+
+        return 0;
+}
--- test.cpp
+++ test.cpp
@@ -2,24 +2,83 @@
 
 using namespace std;
 
+// int main()
+// {
+//     string a = "pqpyyy";
+//     int count1 = 0;
+//     int count2 = 0;
+
+//     for (int i = 0; i < a.size(); i++)
+//     {
+//         if (a[i] == 'p')
+//             count1++;
+//         else if (a[i] == 'y')
+//             count2++;
+//     }
+//     if (count1 == 0 || count2 == 0)
+//         cout << "true" << endl;
+//     else if (count1 == count2)
+//         cout << "true" << endl;
+//     else
+//         cout << "false" << endl;
+//     return 0;
+// }
+
+// int main()
+// {
+//     int number, num;
+//     double rate[10];
+//     cin >> number;
+
+//     for (int i = 0; i < number; i++)
+//     {
+//         double sum = 0;
+//         double average = 0;
+//         double count = 0;
+//         int jumsu[10];
+//         cin >> num;
+//         for (int j = 0; j < num; j++)
+//         {
+//             cin >> jumsu[j];
+//             sum += jumsu[j];
+//         }
+//         average = sum / num;
+//         for (int j = 0; j < num; j++)
+//         {
+//             if (average < jumsu[j])
+//                 count++;
+//         }
+//         rate[i] = count / num * 100;
+//     }
+//     for (int i = 0; i < number; i++)
+//     {
+//         cout << rate[i] << "%" << '\n';
+//     }
+
+//     return 0;
+// }
+
 int main()
 {
-    string a = "pqpyyy";
-    int count1 = 0;
-    int count2 = 0;
-
-    for (int i = 0; i < a.size(); i++)
+    int number;
+    cin >> number;
+    int num[100];
+    int dou[number];
+    for (int i = 0; i < number; i++)
     {
-        if (a[i] == 'p')
-            count1++;
-        else if (a[i] == 'y')
-            count2++;
+        cin >> num[i];
     }
-    if (count1 == 0 || count2 == 0)
-        cout << "true" << endl;
-    else if (count1 == count2)
-        cout << "true" << endl;
-    else
-        cout << "false" << endl;
+    for (int i = 0; i < number; i++)
+    {
+        int count = 0;
+        for (int j = 0; j < number; j++)
+        {
+            if (num[i] == num[j])
+                count++;
+        }
+        if (count < 2)
+            cout << num[i];
+    }
+
     return 0;
 }
